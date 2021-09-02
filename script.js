05295c4eedf97ee004dd273d79ae1ad4790b6488
Michael Soto <michael_nickolas@hotmail.com>
2021-09-02 14:17:58 -0300

    Testando troca do div por ul no html

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let gameImg = document.getElementsByClassName("gameImg");
+// let gameImg = document.getElementsByClassName('gameimg')
 
 let playerPick = 0;
 let playerScore = 0;
 let computerPick = 0;
-let computerScore = 0; // 0: draw - 1: player wins, 2: computer wins
+let computerScore = 0;
+let winner = -1;  // 0: draw - 1: player wins, 2: computer wins 
 
-// 1- Rock
-// 2- Paper
-// 3- Scissor
-let gameLogic = function (playerOption, computerOption) {
-  // computerOption = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+// let gameLogic = function(playerOption, computerOption) {
+  function player(pick) {
+  playerPick = pick; 
+  computerPick = Math.floor(Math.random() * 3 + 1);
+  
 
+  // 1- Rock
+  // 2- Paper
+  // 3- Scissor
   if (
     (playerOption == 1 && computerOption == 1) ||
     (playerOption == 2 && computerOption == 2) ||
@@ -27,59 +31,73 @@ let gameLogic = function (playerOption, computerOption) {
     winner = 2;
   }
   return winner;
-};
-
-let whoWins = function (winner) {
-  let resultMessage = document.getElementById("messageResult");
-  if (winner === 0) {
-    resultMessage.innerHTML = "Draw";
-  } else if (winner === 1) {
-    resultMessage.innerHTML = "You wins !!!";
-    playerScore++;
-    document.getElementById("player-score").innerHTML = `Score: ${playerScore}`;
-  } else if (winner === 2) {
-    resultMessage.innerHTML = "Computer Wins !!!";
-    computer++;
-    document.getElementById("computer-score").innerHTML = `Score: ${computerScore}`;
-  }
-};
-
-for (let i = 0; i < gameImg.length; i++) {
-  // A imagem esmaece quando o usuário passa o mouse sobre ela
-
-  gameImg[i].style.opacity = "0.2";
-  // Adicionando evento de clique às opções do jogador
-  gameImg[i].addEventListener("click", function (element) {
-    let tempId = element.target.parentNode.id;
-    let tempPick = tempId.split("-")[0];
-
-    if (tempPick === "player") {
-      /*	Todas as imagens terão sua opacidade reduzida,
- mas so o que foram selecionados (jogador / robô) */
-      for (let j = 0; j < gameImg.length; j++) {
-        gameImg[j].style.opacity = "0.2";
-      }
-
-      playerPick = tempId.split("-")[2];
-      document.getElementById(
-        "player-pick-" + playerPick
-      ).firstChild.style.opacity = "1.0";
-
-      // Randomiza o jogo do robô toda vez que o jogador joga!!
-      computerPick = Math.floor(Math.random() * 3 + 1);
-      document.getElementById("computer-pick-" + computerPick).firstChild.style.opacity = "1.0";
-
-			console.log('You picks', playerPick);
-			console.log('Computer picks', computerPick);
-
-
-      whoWins(gameLogic(playerPick, computerPick));
-      console.log("-----------------------------");
-    } else {
-      // O jogador não é capaz de selecionar o lado do computador
-      alert("Não permitido, selecione o player 1 !");
-    }
-  });
 }
 
+  document.getElementById("player-pick-1").classList.remove("selected");
+  document.getElementById("player-pick-2").classList.remove("selected");
+  document.getElementById("player-pick-3").classList.remove("selected");
+  document.getElementById("computer-pick-1").classList.remove("selected");
+  document.getElementById("computer-pick-2").classList.remove("selected");
+  document.getElementById("computer-pick-3").classList.remove("selected");
 
+  document.getElementById("player-pick-" + playerPick)
+    .classList.add("selected");
+  document.getElementById("computer-pick-" + computerPick)
+    .classList.add("selected");
+
+  // let whoWins = function (winner) {
+  //   let resultMessage = document.getElementById('messageResult')
+    if(winner === 0) {
+      document.getElementById('messageResult').innerHTML = 'DRAW';
+        // resultMessage.innerHTML = 'DRAW';
+    } else if(winner === 1) {
+      document.getElementById('messageResult').innerHTML = 'Player Wins';
+        // resultMessage.innerHTML = 'Player WINS !!!';
+        document.getElementById('player-score').innerHTML = `Score: ${playerScore}`
+        playerScore++;
+
+    } else if (winner === 2) {
+      document.getElementById('messageResult').innerHTML = 'Computer Wins';
+        // resultMessage.innerHTML = 'Computer WINS !!!';
+        document.getElementById('computer-score').innerHTML = `Score: ${computerScore}`
+        computerScore++;
+    }
+// }
+
+
+
+// for(let i = 0; i < gameImg.length; i++) {
+  
+
+//   gameImg[i].style.opacity = '0.2'
+//   // Adding click event to player's options
+//   gameImg[i].addEventListener('click', function(element) {
+//     console.log('addEventListener')
+//       let tempId = element.target.parentNode.id;
+//       let tempPick = tempId.split('-')[0];
+      
+//       if (tempPick === 'player') {
+//           // All images will have their opacity reduced,
+//           // but the ones that were selected (player/robot)
+//           for(let j = 0; j < gameImg.length; j++) {
+//               gameImg[j].style.opacity = '0.2';
+//           }
+
+//           playerPick = tempId.split('-')[2];
+//           document.getElementById('player-pick-' + playerPick).firstChild.style.opacity = '1.0'
+
+//           // Randomizes robot's play everytime the player plays!
+//           computerPick = Math.floor((Math.random() * 3) + 1)
+//           document.getElementById('computer-pick-' + computerPick).firstChild.style.opacity = '1.0'
+
+//           console.log('Yoshimi picks', playerPick);
+//           console.log('Computer picks', computerPick);
+          
+//           whoWins(gameLogic(playerPick, computerPick));
+//           console.log('-----------------------------');
+//       } else {
+//           // Player is not able to select from the Robot's side
+//           alert('You are NOT the Robot!');
+//       }
+//   })
+// }
